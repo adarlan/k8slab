e3d9c4f7039968ecf2caade0e7eb99667585cdfe
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-09 16:06:08 -0300

    Update local-environment

---
+++ local-environment/modules.tf
@@ -0,0 +1,13 @@
+
+module "kind" {
+  source = "./../kind-cluster"
+  cluster_name = "foo"
+}
+
+module "argo-cd" {
+  source = "./../argo-cd"
+  host = module.kind.endpoint
+  cluster_ca_certificate = module.kind.cluster_ca_certificate
+  client_certificate = module.kind.client_certificate
+  client_key = module.kind.client_key
+}
