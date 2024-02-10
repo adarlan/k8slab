f783838533bb933e6a9604c4cacb83eae0602fb6
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-10 02:10:34 -0300

    Rename local environment

--- environment-local/modules.tf
+++ environment-local/modules.tf
@@ -11,3 +11,11 @@ module "argo-cd" {
   client_certificate = module.kind.client_certificate
   client_key = module.kind.client_key
 }
+
+module "kube-prometheus" {
+  source = "./../kube-prometheus"
+  host = module.kind.endpoint
+  cluster_ca_certificate = module.kind.cluster_ca_certificate
+  client_certificate = module.kind.client_certificate
+  client_key = module.kind.client_key
+}
