0dfb36c5226d643d8e26c06c680be27f8a9d7380
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-10 02:11:51 -0300

    Add EKS environment

---
+++ environment-eks/modules.tf
@@ -0,0 +1,9 @@
+
+module "aws-vpc" {
+  source = "./../aws-vpc"
+  cluster_name = var.cluster_name
+  vpc_cidr_block = var.vpc_cidr_block
+  tags = var.tags
+  max_az_count = var.max_az_count
+  max_nat_gateway_count = var.max_nat_gateway_count
+}
