e8dd77aa9c089ca8c3857dca629ad5aca7320d59
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-10 14:18:16 -0300

    Add Karpenter

---
+++ karpenter/karpenter-helm-release.tf
@@ -0,0 +1,31 @@
+
+resource "helm_release" "karpenter" {
+  name       = "${var.cluster_name}/karpenter"
+
+  namespace        = "karpenter"
+  create_namespace = true
+
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = "v0.16.3"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.karpenter_role.arn
+  }
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "clusterEndpoint"
+    value = var.k8s_auth_credentials.host
+  }
+
+  set {
+    name  = "aws.defaultInstanceProfile"
+    value = aws_iam_instance_profile.karpenter_instance_profile.name
+  }
+}
---
+++ karpenter/variables.tf
@@ -0,0 +1,27 @@
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "k8s_auth_credentials" {
+  type = object({
+    host                   = string
+    cluster_ca_certificate = string
+    client_certificate     = string
+    client_key             = string
+  })
+  sensitive = true
+}
+
+variable "eks_oidc_provider_url" {
+    type = string
+}
+
+variable "eks_oidc_provider_arn" {
+    type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
