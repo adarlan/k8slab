42d4cc5a1bf776879c63b66e672d564ac6076c99
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-10 23:59:38 -0300

    Update kind-ingress module

--- kind-nginx-ingress-controller/helm-release.tf
+++ kind-nginx-ingress-controller/helm-release.tf
@@ -1,7 +1,32 @@
 
 resource "helm_release" "kind_nginx_ingress_controller" {
-  name  = "kind-nginx-ingress-controller"
-  
-  chart = "./../kind-nginx-ingress-controller/helm-chart"
-  # TODO the path is relative from where you run terraform, not from this .tf file
+  name = "ingress-nginx"
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
+  version = "4.7.1"
+
+  namespace = "ingress-nginx"
+  create_namespace = true
+
+  values = [ file("./../kind-nginx-ingress-controller/helm-values.yaml") ]
+  # TODO use ${path.module}?
+}
+
+resource "null_resource" "wait_for_ingress_nginx" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the nginx ingress controller...\n"
+      kubectl --kubeconfig ${pathexpand("./kubeconfig")} wait --namespace ingress-nginx \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=controller \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [ helm_release.kind_nginx_ingress_controller ]
 }
--- kind-nginx-ingress-controller/variables.tf
+++
@@ -1,9 +0,0 @@
-
-variable "cluster_credentials" {
-    type = object({
-        host = string
-        cluster_ca_certificate = string
-        client_certificate = string
-        client_key       = string
-    })
-}
