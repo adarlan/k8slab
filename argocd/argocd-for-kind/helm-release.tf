af5a9fdbee48e909b228436d34413cce77f55e8a
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-14 00:13:47 -0300

    Rename argocd module

--- argocd/argocd-for-kind/helm-release.tf
+++ argocd/argocd-for-kind/helm-release.tf
@@ -1,19 +1,12 @@
-
 resource "helm_release" "argocd" {
   name = "argocd"
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  # version    = "5.27.3"
-  version = "4.9.7"
+  version    = "5.27.3"
 
   namespace        = "argocd"
   create_namespace = true
 
-  # values = [templatefile("./values.yaml", {})]
-  # values = [
-  #     file("argocd/application.yaml")
-  # ]
-
-  # timeout = "1200"
+  values = [ templatefile("${path.module}/helm-values.yaml", {}) ]
 }
