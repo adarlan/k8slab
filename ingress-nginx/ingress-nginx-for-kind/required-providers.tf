5613bf32a8ffd90d889e511f11c062f9eb08d2f0
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-02-14 00:11:29 -0300

    Rename ingress-nginx modules

--- ingress-nginx/ingress-nginx-for-kind/required-providers.tf
+++ ingress-nginx/ingress-nginx-for-kind/required-providers.tf
@@ -1,16 +1,9 @@
 
 terraform {
   required_providers {
-
     helm = {
       source  = "hashicorp/helm"
       version = "2.12.1"
     }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.2"
-    }
-
   }
 }
