42b256d24a8b896e4d92517011c4a95e88efd051
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-03-17 22:07:24 -0300

    Add ca_key output

---
+++ local-cluster/local-exec.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "local_exec" {
+
+  depends_on = [kind_cluster.default]
+
+  provisioner "local-exec" {
+    interpreter = ["sh", "-c"]
+    command     = "docker cp k8slab-control-plane:/etc/kubernetes/pki/ca.key ca.key"
+  }
+}
+
+data "local_file" "ca_key" {
+
+  depends_on = [null_resource.local_exec]
+
+  filename = "${path.module}/ca.key"
+}
--- local-cluster/outputs.tf
+++ local-cluster/outputs.tf
@@ -1,19 +1,24 @@
 output "endpoint" {
   sensitive = true
-  value = kind_cluster.default.endpoint
+  value     = kind_cluster.default.endpoint
+}
+
+output "ca_key" {
+  sensitive = true
+  value     = data.local_file.ca_key.content
 }
 
 output "ca_certificate" {
   sensitive = true
-  value = kind_cluster.default.cluster_ca_certificate
+  value     = kind_cluster.default.cluster_ca_certificate
 }
 
 output "root_user_key" {
   sensitive = true
-  value = kind_cluster.default.client_key
+  value     = kind_cluster.default.client_key
 }
 
 output "root_user_certificate" {
   sensitive = true
-  value = kind_cluster.default.client_certificate
+  value     = kind_cluster.default.client_certificate
 }
