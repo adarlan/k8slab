beb2f23b336262fd7dcff79b7d937c9f23af2c6a
Adarlan Teixeira <adarlanteixeira@outlook.com>
2024-03-23 20:39:06 -0300

    Decrease timeout

--- cluster-toolkit/argocd/helm-release.tf
+++ cluster-toolkit/argocd/helm-release.tf
@@ -2,13 +2,13 @@ resource "helm_release" "argo_cd" {
 
   name = "argo-cd"
 
-  namespace        = "argocd"
+  namespace = "argocd"
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "6.4.1"
 
-  timeout       = 1200
+  timeout       = 300
   wait          = true
   wait_for_jobs = true
 
